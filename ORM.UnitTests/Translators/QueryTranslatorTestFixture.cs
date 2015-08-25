e95335cb2c66010abc7b9f50c9a25b10fefa09b9
thabart <thierry.habart@prodatamobility.com>
2015-08-25 16:49:29 +0200

    Add a new unit test for QueryTranslator

--- ORM.UnitTests/Translators/QueryTranslatorTestFixture.cs
+++ ORM.UnitTests/Translators/QueryTranslatorTestFixture.cs
@@ -1,8 +1,11 @@
 ﻿using Moq;
+
 using NUnit.Framework;
+
 using ORM.Core;
 using ORM.Translators;
 using ORM.UnitTests.Models;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,17 +19,23 @@ namespace ORM.UnitTests.Translators
         {
             protected QueryTranslator QueryTranslator;
 
+            protected string ColumnName;
+
+            protected string TableName;
+
             public override void Arrange()
             {
-                var mappingTranslator = new Mock<IMappingRuleTranslator>();
-                mappingTranslator.Setup(m => m.GetTableName(It.IsAny<Type>())).Returns("TABLE_NAME");
-                mappingTranslator.Setup(m => m.GetColumnName(It.IsAny<Type>(), It.IsAny<string>())).Returns("COLUMN_NAME");
-                QueryTranslator = new QueryTranslator(mappingTranslator.Object);
+                var mappingRuleTranslatorStub = new Mock<IMappingRuleTranslator>();
+                ColumnName = "COLUMN_NAME";
+                TableName = "TABLE_NAME";
+                mappingRuleTranslatorStub.Setup(m => m.GetTableName(It.IsAny<Type>())).Returns(TableName);
+                mappingRuleTranslatorStub.Setup(m => m.GetColumnName(It.IsAny<Type>(), It.IsAny<string>())).Returns(ColumnName);
+                QueryTranslator = new QueryTranslator(mappingRuleTranslatorStub.Object);
             }
         }
 
         [TestFixture]
-        public class WhenTranslateExpressionIntoSqlScript : GivenQueryTranslator
+        public sealed class WhenTranslatingSelectInstructionOneColumnIntoSqlScript : GivenQueryTranslator
         {
             private Expression _selectExpression;
 
@@ -36,6 +45,13 @@ namespace ORM.UnitTests.Translators
             public void ThenSqlScriptIsGenerated()
             {
                 Assert.IsNotEmpty(_sqlScript);
+            }
+
+            [Test]
+            public void ThenSqlScriptIsCorrect()
+            {
+                var expectedResult = string.Format("SELECT {0} FROM {1}", ColumnName, TableName);
+                Assert.That(_sqlScript, Is.EqualTo(expectedResult));
             }
 
             public override void Arrange()
@@ -59,5 +75,51 @@ namespace ORM.UnitTests.Translators
                 _sqlScript = QueryTranslator.Translate(_selectExpression);
             }
         }
+
+        [TestFixture]
+        public sealed class WhenTranslatingSelectInstructionOnTwoColumnsIntoSqlScript : GivenQueryTranslator
+        {
+            private Expression _selectExpression;
+
+            private string _sqlScript;
+
+            [Test]
+            public void ThenSqlScriptIsGenerated()
+            {
+                Assert.IsNotNullOrEmpty(_sqlScript);
+            }
+
+            [Test]
+            public void ThenSqlScriptIsCorrect()
+            {
+                var expectedResult = string.Format("SELECT {0},{1} FROM {2}", ColumnName, ColumnName, TableName);
+                Assert.That(_sqlScript, Is.EqualTo(expectedResult));
+            }
+
+            public override void Arrange()
+            {
+                base.Arrange();
+                var customers = new List<Customer>
+                {
+                    new Customer
+                    {
+                        FirstName = "Thierry",
+                        LastName = "Habart"
+                    }
+                };
+
+                var queryableCustomers = customers.AsQueryable();
+                _selectExpression = queryableCustomers.Select(q => new
+                {
+                    q.FirstName,
+                    q.LastName
+                }).Expression;
+            }
+
+            public override void Act()
+            {
+                _sqlScript = QueryTranslator.Translate(_selectExpression);
+            }
+        } 
     }
 }
