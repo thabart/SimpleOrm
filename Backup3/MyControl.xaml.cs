e6747337b664afdcc906d1e2e0476e8725c0b625
thabart <habarthierry@hotmail.fr>
2015-09-01 22:08:48 +0200

    Upgrade the VSIX project.

---
+++ Backup3/MyControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace ORM.VSPackage
+{
+    /// <summary>
+    /// Interaction logic for MyControl.xaml
+    /// </summary>
+    public partial class MyControl
+    {
+        public MyControl()
+        {
+            InitializeComponent();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(string.Format(System.Globalization.CultureInfo.CurrentUICulture, "We are inside {0}.button1_Click()", this.ToString()),
+                            "My Tool Window");
+
+        }
+    }
+}
