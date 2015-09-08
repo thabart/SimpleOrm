269b07d3441d8c4be103661e5bdb2e83c5afd683
thabart <habarthierry@hotmail.fr>
2015-09-08 22:03:18 +0200

    Generate the DbContext

--- ORM.VSPackage/ORM.VSPackagePackage.cs
+++ ORM.VSPackage/ORM.VSPackagePackage.cs
@@ -144,6 +144,7 @@ namespace ORM.VSPackage
 
             GenerateModels(tableDefinitions, modelProjectItem, templatePath);
             GenerateMappings(tableDefinitions, mappingsProjectItem, templatePath);
+            GenerateDbContext(tableDefinitions, dbContextProjectItem, templatePath);
         }
 
         private static void GenerateModels(
@@ -176,7 +177,7 @@ namespace ORM.VSPackage
 
                     var fieldName = "_" + columnDefinition.ColumnName.ToLower();
                     cls.AddVariable(
-                        "_" + columnDefinition.ColumnName.ToLower(),
+                        fieldName,
                         type,
                         -1,
                         vsCMAccess.vsCMAccessPrivate);
@@ -269,6 +270,87 @@ namespace ORM.VSPackage
                 startPoint.EndOfLine();
                 startPoint.Indent(Count: 2);
             }
+        }
+
+        private static void GenerateDbContext(
+            IEnumerable<TableDefinition> tableDefinitions,
+            ProjectItem dbContextProjectItem,
+            string templatePath) {
+            dbContextProjectItem.ProjectItems.AddFromTemplate(templatePath, "DbContext.cs");
+            var projectItem = dbContextProjectItem.ProjectItems.Item(1);
+            var codeNamespace = GetNameSpaceFromFileCode(projectItem.FileCodeModel);
+            var modelNamespace = codeNamespace.FullName.Replace("DbContext", "Models");
+            var mappingNamespace = codeNamespace.FullName.Replace("DbContext", "Mappings");
+            
+            // Add the import instruction
+            var fileCodeModel = (FileCodeModel2)projectItem.FileCodeModel;
+            fileCodeModel.AddImport("ORM.Mappings");
+            fileCodeModel.AddImport("ORM.Core");
+            fileCodeModel.AddImport(modelNamespace);
+            fileCodeModel.AddImport(mappingNamespace);
+
+            // Add inheritance to BaseDbContext
+            var cls = GetCodeClassFromFileCode(projectItem.FileCodeModel);
+            cls.Access = vsCMAccess.vsCMAccessPublic;
+            cls.AddBase("BaseDbContext");
+
+            // Add fields
+            foreach(var tableDefinition in tableDefinitions)
+            {
+                var fieldName = "_" + tableDefinition.TableName.ToLower();
+                cls.AddVariable(
+                    fieldName,
+                    "IDbSet<" + tableDefinition.TableName + ">",
+                    -1,
+                    vsCMAccess.vsCMAccessPrivate);
+            }
+
+            // Add constructor
+            var constructorStartPoint = cls.GetStartPoint(vsCMPart.vsCMPartBody).CreateEditPoint();
+            constructorStartPoint.LineDown(tableDefinitions.Count());
+            constructorStartPoint.Indent(Count: 1);
+            constructorStartPoint.Insert("public DbContext() : base(\"CustomConnectionString\") { } \n");
+
+            // Add properties
+            foreach (var tableDefinition in tableDefinitions)
+            {
+                var fieldName = "_" + tableDefinition.TableName.ToLower();
+                CodeProperty property = cls.AddProperty(tableDefinition.TableName+"s",
+                    tableDefinition.TableName+"s", 
+                    "IDbSet<" + tableDefinition.TableName + ">",
+                    -1,
+                    vsCMAccess.vsCMAccessPublic,
+                    null);
+
+                var epGetter =
+                    property.Getter.GetStartPoint(vsCMPart.vsCMPartBody).CreateEditPoint();
+                epGetter.Delete(property.Getter.GetEndPoint(vsCMPart.vsCMPartBody));
+                epGetter.Indent();
+                epGetter.Insert(string.Format("return {0}; \n", fieldName));
+                epGetter.Indent(Count: 3);
+
+                var epSetter =
+                    property.Setter.GetStartPoint(vsCMPart.vsCMPartBody).CreateEditPoint();
+                epSetter.Delete(property.Setter.GetEndPoint(vsCMPart.vsCMPartBody));
+                epSetter.Indent();
+                epSetter.Insert(string.Format("{0} = value; \n", fieldName));
+                epSetter.Indent(Count: 3);
+            }
+
+            // Add mappings
+            var mappingEndPoint = cls.GetEndPoint(vsCMPart.vsCMPartBody).CreateEditPoint();
+            mappingEndPoint.Indent(Count : 1);
+            mappingEndPoint.Insert("protected override void Mappings(IEntityMappingContainer entityMappingContainer) { \n");
+            foreach(var tableDefinition in tableDefinitions)
+            {
+                var mappingName = tableDefinition.TableName + "Mapping";
+                mappingEndPoint.Indent(Count: 3);
+                mappingEndPoint.Insert("entityMappingContainer.AddMapping(new " + mappingName + "()); \n");
+                
+            }
+
+            mappingEndPoint.Indent(Count: 2);
+            mappingEndPoint.Insert("} \n");
         }
 
         /// <summary>
