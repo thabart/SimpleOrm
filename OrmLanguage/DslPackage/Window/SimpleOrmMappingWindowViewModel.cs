143673ddc885e9e806410636f86de676a442e688
thabart <habarthierry@hotmail.fr>
2015-09-28 21:11:01 +0200

    Add t4 template to generate entities.

--- OrmLanguage/DslPackage/Window/SimpleOrmMappingWindowViewModel.cs
+++ OrmLanguage/DslPackage/Window/SimpleOrmMappingWindowViewModel.cs
@@ -3,46 +3,6 @@ using System.ComponentModel;
 
 namespace Company.OrmLanguage.Window
 {
-    public class PropertyMappingViewModel : INotifyPropertyChanged
-    {
-        private readonly Property _property;
-
-        private string _columnName;
-
-        public PropertyMappingViewModel(Property property)
-        {
-            _property = property;
-        }
-
-        public string PropertyName { get; set; }
-
-        public string ColumnName
-        {
-            get { return _columnName; }
-            set
-            {
-                _columnName = value;
-                using (var transaction = _property.Store.TransactionManager.BeginTransaction())
-                {
-                    _property.ColumnName = value;
-                    transaction.Commit();
-                }
-
-                NotifyPropertyChanged("ColumnName");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void NotifyPropertyChanged(string info)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(info));
-            }
-        }
-    }
-
     public sealed class SimpleOrmMappingWindowViewModel : INotifyPropertyChanged
     {
         private EntityElement _entityElement;
