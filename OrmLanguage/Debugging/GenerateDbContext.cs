78fc13e86ba31823a8e338508224b0175df34b79
thabart <thierry.habart@prodatamobility.com>
2015-09-29 16:35:32 +0200

    Generate the DBContext.

---
+++ OrmLanguage/Debugging/GenerateDbContext.cs
@@ -0,0 +1,25 @@
+﻿
+ using ORM.Core;
+ using ORM.Mappings;
+
+ using Debugging.Mappings;
+ using Debugging.Models;
+
+ namespace Debugging 
+ {
+	public class DbContext : BaseDbContext 
+	{
+		public DbContext() : base("CustomConnectionString")
+		{
+		}
+
+		public IDbSet<Deeeevvit> Deeeevvits { get; set; }
+		public IDbSet<Loki> Lokis { get; set; }
+
+		public override void Mappings(IEntityMappingContainer entityMappingContainer)
+		{
+			entityMappingContainer.AddMapping(new DeeeevvitMapping());
+			entityMappingContainer.AddMapping(new LokiMapping());
+		}
+	}
+ }
